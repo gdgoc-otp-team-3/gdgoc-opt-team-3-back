331e561bf8ed0e95fd1a6af53d2df3d4131965c5
minguni <80194344+min-guni@users.noreply.github.com>
2025-11-30 12:38:13 +0900

    feature : spring init

---
+++ src/main/kotlin/gdgoc_otp_team_3/backend/BackendApplication.kt
@@ -0,0 +1,11 @@
+package gdgoc_otp_team_3.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendApplication>(*args)
+}
